8ab08bb8f5b60a55a451364a72764a79507c1675
Syauqi <syauqisnh@gmail.com>
2023-04-08 15:57:42 +0700

    Halaman Documentation

--- src/components/C_Dokumentation.js
+++ src/components/C_Dokumentation.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { BsFillFolderFill } from "react-icons/bs";
 import Sidebar from "../components/Sidebar";
 
-export const Dokumentation = () => {
+const Dokumentation = () => {
+  const kotakStyle = {
+    width: "280px",
+    height: "280px",
+    backgroundColor: "#D9D9D9",
+    borderRadius: "50px",
+    margin: "10px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column", // tambahkan flex-direction column
+  };
+
+  const imgStyle = {
+    fontSize: "6em",
+  };
+
+  const textStyle = {
+    marginTop: "10px", // beri margin top pada text
+    textAlign: "center",
+  };
+
+  const kotakList1 = [
+    { id: 1, icon: <BsFillFolderFill />, text: <><div>Dokumen</div><div>Perencanaan</div></> },
+    { id: 2, icon: <BsFillFolderFill />, text: <><div>Dokumen</div><div>Evaluasi Operasi</div></> },
+    { id: 3, icon: <BsFillFolderFill />, text: <><div>Dokumen</div><div>Profil Kelistrikkan</div></> },
+  ];
+  
+  const kotakList2 = [
+    { id: 4, icon: <BsFillFolderFill />, text: <><div>Dokumen</div><div>SOP Pengoprasian</div></> },
+    { id: 5, icon: <BsFillFolderFill />, text: <><div>Dokumen</div><div>Single Line Diagram</div></> },
+  ];
+
   return (
     <>
       <Sidebar />
@@ -10,6 +43,24 @@ export const Dokumentation = () => {
           <div className="body flex-grow-1 px-3">
             <div className="header">
               <p>Documentation</p>
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <div style={{ display: "flex" }}>
+                  {kotakList1.map((kotak) => (
+                    <div key={kotak.id} style={kotakStyle}>
+                      {React.cloneElement(kotak.icon, { style: imgStyle })}
+                      <p style={textStyle}>{kotak.text}</p> {/* tambahkan text */}
+                    </div>
+                  ))}
+                </div>
+                <div style={{ display: "flex" }}>
+                  {kotakList2.map((kotak) => (
+                    <div key={kotak.id} style={kotakStyle}>
+                      {React.cloneElement(kotak.icon, { style: imgStyle })}
+                      <p style={textStyle}>{kotak.text}</p> {/* tambahkan text */}
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
