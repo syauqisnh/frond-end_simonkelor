b5446d9a3af64ebe76e57aee8b8b34ba43189487
Syauqi <syauqisnh@gmail.com>
2023-04-08 20:42:22 +0700

    Update

--- src/components/C_Dokumentation.js
+++ src/components/C_Dokumentation.js
@@ -4,35 +4,88 @@ import Sidebar from "../components/Sidebar";
 
 const Dokumentation = () => {
   const kotakStyle = {
-    width: "280px",
-    height: "280px",
+    width: "300px", // ubah lebar kotak menjadi 300px
+    height: "300px", // ubah tinggi kotak menjadi 300px
     backgroundColor: "#D9D9D9",
     borderRadius: "50px",
-    margin: "10px",
+    margin: "30px 20px 10px 10px",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "column", // tambahkan flex-direction column
+    flexDirection: "column",
+  };
+
+  const kotakStyleMobile = {
+    ...kotakStyle,
+    width: "300px", // ubah lebar kotak menjadi 300px
+    margin: "30px 20px 10px 10px",
+    marginTop: "30px",
   };
 
   const imgStyle = {
     fontSize: "6em",
+    color: "#8473ED",
   };
 
   const textStyle = {
-    marginTop: "10px", // beri margin top pada text
+    marginTop: "15px",
     textAlign: "center",
+    fontSize: "15pt",
   };
-
   const kotakList1 = [
-    { id: 1, icon: <BsFillFolderFill />, text: <><div>Dokumen</div><div>Perencanaan</div></> },
-    { id: 2, icon: <BsFillFolderFill />, text: <><div>Dokumen</div><div>Evaluasi Operasi</div></> },
-    { id: 3, icon: <BsFillFolderFill />, text: <><div>Dokumen</div><div>Profil Kelistrikkan</div></> },
+    {
+      id: 1,
+      icon: <BsFillFolderFill />,
+      text: (
+        <>
+          <div>Dokumen</div>
+          <div>Perencanaan</div>
+        </>
+      ),
+    },
+    {
+      id: 2,
+      icon: <BsFillFolderFill />,
+      text: (
+        <>
+          <div>Dokumen</div>
+          <div>Evaluasi Operasi</div>
+        </>
+      ),
+    },
+    {
+      id: 3,
+      icon: <BsFillFolderFill />,
+      text: (
+        <>
+          <div>Dokumen</div>
+          <div>Profil Kelistrikkan</div>
+        </>
+      ),
+    },
   ];
-  
+
   const kotakList2 = [
-    { id: 4, icon: <BsFillFolderFill />, text: <><div>Dokumen</div><div>SOP Pengoprasian</div></> },
-    { id: 5, icon: <BsFillFolderFill />, text: <><div>Dokumen</div><div>Single Line Diagram</div></> },
+    {
+      id: 4,
+      icon: <BsFillFolderFill />,
+      text: (
+        <>
+          <div>Dokumen</div>
+          <div>SOP Pengoprasian</div>
+        </>
+      ),
+    },
+    {
+      id: 5,
+      icon: <BsFillFolderFill />,
+      text: (
+        <>
+          <div>Dokumen</div>
+          <div>Single Line Diagram</div>
+        </>
+      ),
+    },
   ];
 
   return (
@@ -48,15 +101,19 @@ const Dokumentation = () => {
                   {kotakList1.map((kotak) => (
                     <div key={kotak.id} style={kotakStyle}>
                       {React.cloneElement(kotak.icon, { style: imgStyle })}
-                      <p style={textStyle}>{kotak.text}</p> {/* tambahkan text */}
+                      <p style={textStyle}>{kotak.text}</p>{" "}
+                      {/* tambahkan text */}
                     </div>
                   ))}
                 </div>
                 <div style={{ display: "flex" }}>
                   {kotakList2.map((kotak) => (
-                    <div key={kotak.id} style={kotakStyle}>
+                    <div key={kotak.id} style={kotakStyleMobile}>
+                      {" "}
+                      {/* gunakan kotakStyleMobile untuk media screen di bawah 768px */}
                       {React.cloneElement(kotak.icon, { style: imgStyle })}
-                      <p style={textStyle}>{kotak.text}</p> {/* tambahkan text */}
+                      <p style={textStyle}>{kotak.text}</p>{" "}
+                      {/* tambahkan text */}
                     </div>
                   ))}
                 </div>
