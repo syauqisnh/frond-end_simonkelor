d4b15c694957b4165fca3ce51bd384c023147c90
rayyaalika <rayyaalika@gmail.com>
2023-05-24 09:03:15 +0700

    update

--- src/components/C_DataOperasi.js
+++ src/components/C_DataOperasi.js
@@ -141,12 +141,10 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {
-  CContainer,
   CCard,
   CCardBody,
   CCol,
   CRow,
-  CBadge,
   CTable,
   CTableBody,
   CTableDataCell,
